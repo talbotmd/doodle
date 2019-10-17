03de4ca2ffeedea94353865c89ceb5ce6068ba51
Manu Agarwal <Manu@locals-MacBook-Pro.local>
2019-10-16 20:34:30 -0700

    initial scratch files for code usez

---
+++ HyperparameterizedModel.py
@@ -0,0 +1,12 @@
+# Package imports
+import numpy as np
+import matplotlib.pyplot as plt
+from testCases_v2 import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+%matplotlib inline
+
+np.random.seed(1) # set a seed so that the results are consistent
