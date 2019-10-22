bf0e774583e19ac2c9fdf4c2e24f026d0e27d502
Manu Agarwal <Manu@locals-MacBook-Pro.local>
2019-10-21 18:33:31 -0700

    Added training loops to the GeneratorDiscriminator file

--- GeneratorDiscriminator.py
+++ GeneratorDiscriminator.py
@@ -6,23 +6,30 @@ from tensorflow.python.framework import ops
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-layers_dims_Hp2d = [10, 9, 8, 7, 6, 5, 5, 6, 7, 8, 9, 10]
-layers_dims_Dh = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-layers_dims_Dm = [20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 1]
+# layers_dims_Hp2d = [10, 9, 8, 7, 6, 5, 5, 6, 7, 8, 9, 10]
+# layers_dims_Dh = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+# layers_dims_Dm = [20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 1]
 
-A = [[1, 3], [2, 6], [3, 9], [4, 12], [5, 15], [6, 18], [7, 21], [8, 24], [9, 27], [10, 30]]
-B = [[1, 1], [2, 3], [5, 8], [13, 21], [34, 55], [89, 13], [21, 34], [55, 89], [5, 8], [13, 21]]
+
+
+
+def load_dataset():
+    train_dataset = h5py.File('data/sketchy/cat.hdf5', "r")
+    train_set_x_orig = np.array(train_dataset["image_dataset"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["sketch_dataset"][:]) # your train set labels
+    train_set_x_flat = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
+    train_set_y_flat = train_set_y_orig.reshape(train_set_y_orig.shape[0],-1).T
+    return train_set_x_flat/255, train_set_y_flat/255
+
+A, B = load_dataset()
+
+layers_dims_Hp2d = [X.shape[0], 9, 8, 7, 6, 5, 5, 6, 7, 8, 9, X.shape[0]]
+layers_dims_Dh = [X.shape[0], 9, 8, 7, 6, 5, 4, 3, 2, 1]
+layers_dims_Dm = [X.shape[0]+Y.shape[0], 18, 16, 14, 12, 10, 8, 6, 4, 2, 1]
 
 learning_rate = 1
 m = len(A[0])
 k = 10 #Number of interations of dicriminator training before training generator
-#L_dim_H = [10, 9, 8, 7, 6, 5, 5, 6, 7, 8, 9, 10]
-#L_dim_Dh = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-#L_dim_Dm = [20, 18, 16, 14, 12, 10, 8, 6, 4, 2, 1]
-
-#Inputs = {'P': X, 'D_real': Y}
-#print("Inputs: " + str(Inputs))
-
 
 def create_placeholders(n_P, n_D):
     P = tf.placeholder(tf.float32,[n_P,None], name='P') #Input Photo
@@ -96,7 +103,7 @@ def Hp2d_loss(A_human_fake, A_match_fake):
 
 #Start Trying out the code
 ops.reset_default_graph() #reset default graph
-P, D_real = create_placeholders(10, 10)
+P, D_real = create_placeholders(X.shape[0], X.shape[0])
 print ("P = " + str(P))
 print ("D_real = " + str(D_real))
 print ("layers_dims_Hp2d = " + str(layers_dims_Hp2d))
