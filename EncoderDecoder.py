fix: so I fixed several bugs in the encoder decoder code, but there is still an issue with the loss function I think.  Seems like it doesn't create the gradients.  There is also a new file to create a dataset with negative examples

EncoderDecoder.py
@@ -9,7 +9,7 @@ def encoder_layer(num_filters, apply_batchnorm=True,apply_dropout=False, dropout
     initializer = tf.random_normal_initializer(0., 0.02)
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Conv2D(num_filters,3,strides=1,padding='same',kernel_initializer=initializer,use_bias=False))
-        model.dd(keras.layers
+    model.add(tf.keras.layers
         .MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid'))
         
     if apply_batchnorm:
@@ -25,13 +25,13 @@ def decoder_layer(num_filters, apply_batchnorm=True,apply_dropout=False, dropout
     model.add(tf.keras.layers.Conv2DTranspose(num_filters,3,strides=2,padding='same',kernel_initializer=initializer,use_bias=False))
     
     if apply_batchnorm:
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.LeakyReLU())
+        model.add(tf.keras.layers.BatchNormalization())
+        model.add(tf.keras.layers.LeakyReLU())
     if apply_dropout:
-    model.add(tf.keras.layers.Dropout(dropout_prob))
+        model.add(tf.keras.layers.Dropout(dropout_prob))
     return model
 
-def EncoderP2E:
+def EncoderP2E():
     inputs = tf.keras.layers.Input(shape=[256,256,3])
     outputs = inputs
     for i in range(2):
@@ -48,7 +48,7 @@ def EncoderP2E:
     outputs = layer(outputs)
     return tf.keras.Model(inputs=inputs, outputs=outputs, name = "EncoderP2E")
     
-def EncoderD2E:
+def EncoderD2E():
     inputs = tf.keras.layers.Input(shape=[256,256,6])
     outputs = inputs
     for i in range(2):
@@ -72,34 +72,37 @@ def load_minibatch(num=1000, start=-1, n=5):
     # Assumes the in output.hdf5 pairs are already shuffled and randomized. negative examples are created by adding a
     train_dataset = h5py.File('output.hdf5', "r")
     if start < 0:
-    start = random.randint(0,9000-n)
+        start = random.randint(0,2000-n)
     train_set_P_orig = np.array(train_dataset["image_dataset"][start:start+num],dtype='float32')
-    train_set_D_orig_true = np.array(train_dataset["sketch_dataset"][start:start+num],dtype='float32')
+    train_set_D_orig = np.array(train_dataset["sketch_dataset"][start:start+num],dtype='float32')
     
     train_set_P = train_set_P_orig
     train_set_D = train_set_D_orig
-    y_train = np.ones([1, num],dtype=np.float32)
+    y_train = np.ones([num,1],dtype='float32')
     
-    for i in range(1, n):
-        train_set_D_orig_false =  np.array(train_dataset["sketch_dataset"][start+i:start+num+i],dtype='float32') # your train set labels
-        train_set_P = tf.concat([train_set_P, train_set_P_orig], 3)
-        train_set_D = np.concat([train_set_D, train_set_D_orig_false], 3)
-        y_train = np.concat([y_train, np.zeroes([1, num],dtype=np.float32)],axis=2)
+    #for i in range(1, n):
+    train_set_D_orig_false =  np.array(train_dataset["bad_sketch_dataset"][start:start+num],dtype='float32') # your train set labels
+    train_set_P = tf.concat([train_set_P, train_set_P_orig], 0)
+    train_set_D = tf.concat([train_set_D, train_set_D_orig_false], 0)
+    y_train = tf.concat([y_train, np.zeros([num,1],dtype='float32')],axis=0)
+    print("y_train:",y_train.shape)
     return train_set_P/255, train_set_D/255, y_train
 
 
 def EmbeddingCost(E_P, E_D, y_train,margin, n):
     # E_P is embedding output of picture, E_D is embedding output of Doodle, y_train indiciates whether it is positive or negative example, margin is triplet margin, n is number fo negaitve examples per positive example
+    
+    # Theres a problem with this.  I think we need to use a keras layer to compute the cost
     m = len(y_train)
-    loss_fn = 1/m*(np.sum(((1+1/n)*y_train - 1/n)*tf.norm(E_D-E_P,axis=1,ord=2)))+margin
+    loss_fn = 1/m*(tf.keras.backend.sum(((1+1/n)*y_train - 1/n)*tf.norm(E_D-E_P,axis=1,ord=2)))+margin
         # This should give norm(ED-EP) when y = 1, and 1/n*norm(ED_EP) when y is 0.
     return loss_fn
     
 def main():
         #noise = tf.random.normal([1,256,256,3])
-        batch_size = 5 #Normally 1000
+        batch_size = 2 #Normally 1000
         batch_num = 1 #Normally 100
-        n = 5 #Number of
+        n = 1 #Number of
         #test_x, test_y = load_minibatch(num=5, start=0)
         Model_EncD2E = EncoderD2E()
         Model_EncP2E = EncoderP2E()
@@ -110,7 +113,7 @@ def main():
         Encoding_losses = []
         
         for epoch in range(3000):
-            train_P, train_D, train_y = load_minibatch(batch_size, start=-1, n=5)
+            train_P, train_D, y_train = load_minibatch(batch_size, start=-1, n=5)
             print("epoch: ", epoch)
             average_encoding_cost = 0
             average_D2E_cost = 0
@@ -119,11 +122,11 @@ def main():
                 print("image: ", image)
                 with tf.GradientTape() as P2E_tape, tf.GradientTape() as D2E_tape:
                 # Find codes for Photos
-                    P_codes = Model_EncP2E(train_P[image:min(image+batch_size,train_x.shape[0]-1)],training=True)
+                    P_codes = Model_EncP2E(train_P[image:min(image+batch_size,train_P.shape[0]-1)],training=True)
                     # Find codes for Doodles
-                    D_codes = Model_EncP2E(train_D[image:min(image+batch_size,train_x.shape[0]-1)],training=True)
+                    D_codes = Model_EncP2E(train_D[image:min(image+batch_size,train_D.shape[0]-1)],training=True)
                 #Loss
-                    encodingLoss = EmbeddingCost(P_codes, D_codes, y_train, margin=1, n)
+                    encodingLoss = EmbeddingCost(P_codes, D_codes, y_train, 1, n)
                 
                 #Tracking loss
                     average_encoding_cost += encodingLoss
@@ -182,3 +185,7 @@ def main():
         # Save a checkpoint every other iteration
         #if epoch % 2 == 0:
         #    checkpoint.save(file_prefix=checkpoint_prefix)
+
+
+if __name__ == "__main__":
+    main()